db013747cf2652b8ccff98e37d7eb7efe98e5f71
Jishnu <jishnunbhattathiri@gmail.com>
2023-06-26 23:54:20 +0530

    gitignore added

--- kid-candie-problem/target/debug/deps/kid_candie_problem-dd1518102cba4206.d
+++
@@ -1,5 +0,0 @@
-/home/edo/exps/rust/leetcode-75/kid-candie-problem/target/debug/deps/kid_candie_problem-dd1518102cba4206.rmeta: src/main.rs
-
-/home/edo/exps/rust/leetcode-75/kid-candie-problem/target/debug/deps/kid_candie_problem-dd1518102cba4206.d: src/main.rs
-
-src/main.rs:
--- kid-candie-problem/target/debug/kid-candie-problem.d
+++
@@ -1,1 +0,0 @@
-/home/edo/exps/rust/leetcode-75/kid-candie-problem/target/debug/kid-candie-problem: /home/edo/exps/rust/leetcode-75/kid-candie-problem/src/main.rs
