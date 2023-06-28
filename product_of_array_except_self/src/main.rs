e6324e1ea4dd4d63979c44d584fb44f286ac48f9
Jishnu <jishnu.nb@neoito.com>
2023-06-28 16:15:22 +0530

    bug fix

--- product_of_array_except_self/src/main.rs
+++ product_of_array_except_self/src/main.rs
@@ -5,10 +5,23 @@ fn main() {
     println!("{:?}", b);
 }
 fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-    let o: Vec<i32> = vec![];
-    let i = 0;
-    let len = nums.len() as i32;
-    while i < len {
-        i = i + 1;
+    let len = nums.len();
+    let mut o: Vec<i32> = vec![1; len as usize];
+    let mut r = 1;
+    let mut l = 1;
+    let mut j = 0;
+    let mut k = len - 1;
+    loop {
+        println!("k is {} and j is {}", k, j);
+        o[j] = l * o[j];
+        o[k] = o[k] * r;
+        l *= nums[j];
+        r *= nums[len - j - 1];
+        if k == 0 {
+            break;
+        }
+        j += 1;
+        k -= 1;
     }
+    return o;
 }
